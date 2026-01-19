8eb1ea1285f41311da4357b6d7906b810a1d0cdc
chidozie nnam <chidozinnam@gmail.com>
2026-01-19 19:54:57 +0100

    Fix image and API config for production backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,11 @@
 /** @type {import('next').NextConfig} */
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "scalable-ecommerce-website-backend-api.onrender.com",
-        port: "", // leave empty for standard https
-        pathname: "/img/products/**",
-      },
-    ],
-  },
-
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${API_URL}/api/v1/:path*`,
+        destination: `https://scalable-ecommerce-website-backend.onrender.com/api/:path*`,
       },
     ];
   },
