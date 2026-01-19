c4c6a096302a3c058029cf91230b37b4dd1a84f9
chidozie nnam <chidozinnam@gmail.com>
2026-01-20 00:47:53 +0100

    fix(frontend): correct register page to pass separate args to register

--- app/context/auth-context.js
+++ app/context/auth-context.js
@@ -62,7 +62,7 @@ export function AuthProvider({ children }) {
         name,
         email,
         password,
-        passwordConfirm,
+        passwordConfirm: confirm || password,
       });
       return data;
     } finally {
